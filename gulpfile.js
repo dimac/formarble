d9d0519f145a7e7c2caaa9c858991fd0cc56f238
Dima Chukhai <dima@luckyteam.co.uk>
2014-07-11 13:17:10 +0300

    Gulp version command

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,15 @@ var fs = require('fs'),
     path = require('path');
 
 var gulp = require('gulp'),
+    gutil = require('gulp-util'),
     chug = require('gulp-chug'),
     rename = require('gulp-rename'),
     ngmin = require('gulp-ngmin'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    bump = require('gulp-bump');
 
-var controlsDir = 'controls',
+var args = require('minimist')(process.argv.slice(3)),
+    controlsDir = 'controls',
     controls = fs.readdirSync(controlsDir)
         .filter(function (name) {
             return fs.statSync(path.join(controlsDir, name)).isDirectory();
@@ -34,5 +37,32 @@ gulp.task('build-controls', function () {
 
     return stream;
 });
+
+gulp.task('version', function() {
+    var packages = ['package.json', 'bower.json'];
+
+    if (args.set) {
+        return gulp.src(packages)
+            .pipe(bump({version: args.set}))
+            .pipe(gulp.dest('.'));
+    }
+
+    var versions = {
+        patch: 'patch',
+        minor: 'minor',
+        major: 'major',
+        pre: 'prerelease'
+    };
+
+    for(var i in versions) {
+        if(args[i]) {
+            return gulp.src(packages)
+                .pipe(bump({type: versions[i]}))
+                .pipe(gulp.dest('.'));
+        }
+    }
+
+    gutil.log(gutil.colors.yellow('Version not bumped'));
+})
 
 gulp.task('default', ['build-core', 'build-controls']);
