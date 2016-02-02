c47e3eef826e26ab09b382117d1d165acfb1a2f8
nikolas <kolyanechay1991@gmail.com>
2016-02-02 11:18:00 +0200

    Added templates for bs4 common

--- build/tree/tree.js
+++ build/tree/tree.js
@@ -86,11 +86,11 @@ angular.module('formarble/controls/tree').run(['$templateCache', function($templ
 }]);
 
 angular.module('formarble/controls/tree').run(['$templateCache', function($templateCache) {
-  $templateCache.put('bs-4/tree-sidebar.html',
+  $templateCache.put('bs4/tree-sidebar.html',
     '<li ng-repeat="i in $items" ng-class="{selected: $tree.isSelected(i), open: $tree.isOpen(i)}"><a href="" ng-click="$select(i)">{{i.title}}</a><ul ng-if="$tree.isOpen(i)" fm-tree-sidebar="i"></ul></li>');
 }]);
 
 angular.module('formarble/controls/tree').run(['$templateCache', function($templateCache) {
-  $templateCache.put('bs-4/tree.html',
+  $templateCache.put('bs4/tree.html',
     '<h3 class="text-muted" ng-if="!$isSubtree"><a href="" ng-click="$tree.select()">{{$control.title}}</a></h3><div class="row"><div class="col-lg-3" ng-if="!$isSubtree"><ol fm-tree-sidebar="$control"></ol></div><div ng-class="{\'col-lg-12\': $isSubtree, \'col-lg-9\': !$isSubtree}"><div ng-if="!$tree.selected && $subControls.length"><div ng-repeat="c in $subControls" fm-control="c"></div></div><div ng-if="!$tree.selected && !$subControls.length"><h3 class="text-muted">Select option in tree</h3></div><div ng-if="$tree.selected"><div fm-control="$tree.selected"></div></div></div></div>');
 }]);
